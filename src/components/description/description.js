363206a38abda11e1ef9ea2be23cc3d4045c8fca
ivanleskin <livan.skin@yandex.ru>
2021-10-04 20:37:29 +0300

    add new description

--- src/components/description/description.js
+++ src/components/description/description.js
@@ -9,7 +9,7 @@ const Description = () => {
                 <img className="card-img-top mx-auto" src="https://sun9-28.userapi.com/impg/if9DvslZ6iCVgKzbPbLXJAXBDYkQmNG31a6Geg/4qzxiKY4mvQ.jpg?size=786x1080&quality=96&sign=c57dc724cda6d29c86a6c221a6625ec0&type=album"/>
                 <div className="card-body">
                     <h5 className="card-text">Здравствуйте, я Лескин Иван Алексеевич. Мне 21 год и на данный момент я учусь на 2 курсе факультета ФПМИ в МФТИ. Интересуюсь бэкенд разработкой на С++, Python + Django, также разработкой ПО для встроенных систем на С. Ну и эта визитка написана на React)
-                        С людьми разговаривать умею, в команде работать умею, открытый к общению и диалогу
+                        С людьми разговаривать и находить общий язык умею и люблю, хорошо работаю в команде, открыт к общению и диалогу
                     </h5>
                 </div>
             </div>
